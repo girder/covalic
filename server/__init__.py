18f66fd5ea5bfcfeaff482ca15be4889ddf7384c
Michael Grauer <michael.grauer@kitware.com>
2014-11-24 16:17:43 -0500

    initial work on custom web client

--- server/__init__.py
+++ server/__init__.py
@@ -20,7 +20,105 @@
 from .rest import phase
 
 
+
+
+import mako
+import os
+
+
+
+from girder import constants
+
+
+class CustomAppRoot(object):
+    """
+    The webroot endpoint simply serves the main index HTML file of covalic.
+    """
+    exposed = True
+
+    indexHtml = None
+
+    vars = {
+        'plugins': [],
+        'apiRoot': '/api/v1',
+        'staticRoot': '/static',
+        'title': 'Covalic'
+    }
+
+    template = r"""
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>${title}</title>
+        <link rel="stylesheet"
+              href="//fonts.googleapis.com/css?family=Droid+Sans:400,700">
+        <link rel="stylesheet"
+              href="${staticRoot}/lib/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet"
+              href="${staticRoot}/lib/bootstrap/css/bootstrap-switch.min.css">
+        <link rel="stylesheet"
+              href="${staticRoot}/lib/fontello/css/fontello.css">
+        <link rel="stylesheet"
+              href="${staticRoot}/lib/fontello/css/animation.css">
+        <link rel="stylesheet"
+              href="${staticRoot}/lib/jqplot/css/jquery.jqplot.min.css">
+        <link rel="stylesheet"
+              href="${staticRoot}/built/app.min.css">
+        <link rel="icon"
+              type="image/png"
+              href="${staticRoot}/img/Girder_Favicon.png">
+
+        % for plugin in pluginCss:
+            <link rel="stylesheet"
+                  href="${staticRoot}/built/plugins/${plugin}/plugin.min.css">
+        % endfor
+      </head>
+      <body>
+      Welcome to the hungle
+        <div id="g-global-info-apiroot" class="hide">${apiRoot}</div>
+        <div id="g-global-info-staticroot" class="hide">${staticRoot}</div>
+        <script src="${staticRoot}/built/libs.min.js"></script>
+        <script src="${staticRoot}/built/app.min.js"></script>
+
+        % for plugin in pluginJs:
+          <script src="${staticRoot}/built/plugins/${plugin}/plugin.min.js">
+          </script>
+        % endfor
+      </body>
+    </html>
+    """
+
+    def GET(self):
+        if self.indexHtml is None:
+            self.vars['pluginCss'] = []
+            self.vars['pluginJs'] = []
+            builtDir = os.path.join(constants.ROOT_DIR, 'clients', 'web',
+                                    'static', 'built', 'plugins')
+            for plugin in self.vars['plugins']:
+                if os.path.exists(os.path.join(builtDir, plugin,
+                                               'plugin.min.css')):
+                    self.vars['pluginCss'].append(plugin)
+                if os.path.exists(os.path.join(builtDir, plugin,
+                                               'plugin.min.js')):
+                    self.vars['pluginJs'].append(plugin)
+
+            self.indexHtml = mako.template.Template(self.template).render(
+                **self.vars)
+
+        return self.indexHtml
+
+    def updateHtmlVars(self, vars):
+        """
+        If any of the variables in the index html need to change, call this
+        with the updated set of variables to render the template with.
+        """
+        self.vars.update(vars)
+        self.indexHtml = None
+
+
 def load(info):
     phaseExt = phase.Phase()
     info['apiRoot'].challenge_phase.route(
         'POST', (':id', 'submission'), phaseExt.postSubmission)
+    info['serverRoot'], info['serverRoot'].girder = CustomAppRoot(), info['serverRoot']
+    info['serverRoot'].api = info['serverRoot'].girder.api
