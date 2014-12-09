36c873a7c84732741b1bf95772b3113ff99becf4
Michael Grauer <michael.grauer@kitware.com>
2014-12-09 12:42:19 -0500

    override navItems parent function

--- web_external/js/views/layout/GlobalNavView.js
+++ web_external/js/views/layout/GlobalNavView.js
@@ -1,26 +1,16 @@
 covalic.views.LayoutGlobalNavView = girder.views.LayoutGlobalNavView.extend({
 
-    render: function () {
-        var navItems = [{
-            name: 'Challenges',
-            icon: 'icon-sitemap',
+        navItems: function () {
+            var navItems = [{
+                name: 'Challenges',
+                icon: 'icon-sitemap',
             target: 'challenges'
         }, {
             name: 'Users',
             icon: 'icon-user',
             target: 'users'
         }];
-
-        this.$el.html(jade.templates.layoutGlobalNav({
-            navItems: navItems
-        }));
-
-        if (Backbone.history.fragment) {
-            this.$('[g-target="' + Backbone.history.fragment + '"]')
-                .parent().addClass('g-active');
-        }
-
-        return this;
-    }
+       return navItems;
+    },
 
 });
