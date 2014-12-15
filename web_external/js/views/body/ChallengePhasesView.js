44fc8e8315b5d6862a6dda6290e866bf61bafed5
Michael Grauer <michael.grauer@kitware.com>
2014-12-15 14:20:40 -0500

    challenge list and phase pages

---
+++ web_external/js/views/body/ChallengePhasesView.js
@@ -0,0 +1,29 @@
+covalic.views.ChallengePhasesView = covalic.View.extend({
+
+    events: {
+        'click a.c-phase-link': function (event) {
+            var cid = $(event.currentTarget).attr('c-phase-cid');
+            covalic.router.navigate('phase/' + this.collection.get(cid).id, {trigger: true});
+        }
+    },
+
+    initialize: function (settings) {
+        girder.cancelRestRequests('fetch');
+        this.collection = new covalic.collections.ChallengePhaseCollection();
+        this.collection.on('g:changed', function () {
+            this.render();
+        }, this).fetch({
+            challengeId: settings.challenge.get('_id')
+        });
+    },
+
+    render: function () {
+        console.log("cpv render");
+        console.log(this.collection.models);
+        this.$el.html(covalic.templates.challengePhasesPage({
+            phases: this.collection.models
+        }));
+
+        return this;
+    }
+});
