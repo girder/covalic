a835992c23648b802cfc6d9570ed562b686e5579
Zach Mullen <zach.mullen@kitware.com>
2015-01-29 21:57:45 -0500

    Add nav icons to header bar

--- web_external/js/views/layout/HeaderView.js
+++ web_external/js/views/layout/HeaderView.js
@@ -9,6 +9,11 @@ covalic.views.LayoutHeaderView = covalic.View.extend({
     render: function () {
         this.$el.html(covalic.templates.layoutHeader());
 
+        this.$('a[title]').tooltip({
+            placement: 'bottom',
+            delay: {show: 300}
+        });
+
         new covalic.views.LayoutHeaderUserView({
             el: this.$('.c-current-user-wrapper'),
             parentView: this
