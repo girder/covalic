8690ffa120628823e9266a8b82c82642b54ca65c
Michael Grauer <michael.grauer@kitware.com>
2015-02-10 18:52:11 -0500

    Add vault-password to update script

--- ansible/utils/update.sh
+++ ansible/utils/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # argv:
 # $1: pod name
-ansible-playbook provision.yml -i pod_inventory/$1_pod -e pod=$1 -t girder-update
+ansible-playbook provision.yml -i pod_inventory/$1_pod -e pod=$1 -t girder-update --vault-password vault-password.txt
