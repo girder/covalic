girder/covalic

commit 41d1b902c2d2c029f76e070fb0b0925a5c9544ca

Use separate router, fix challenge view navigation

web_external/js/views/body/ChallengeView.js
@@ -15,24 +15,25 @@ covalic.views.ChallengeView = covalic.View.extend({
         }
     },
 
-    render: function() {
-        this.$el.html(girder.templates.challengePage({
+    render: function () {
+        this.$el.html(covalic.templates.challengePage({
             challenge: this.model
         }));
 
         return this;
     }
-
 });
 
-girder.router.route('challenge/:id', 'challenge', function(id, params) {
+covalic.router.route('challenge/:id', 'challenge', function(id, params) {
     // Fetch the challenge by id, then render the view.
     var challenge = new girder.models.ChallengeModel();
     challenge.set({
         _id: id
     }).on('g:fetched', function () {
-        girder.events.trigger('g:navigateTo', covalic.views.ChallengeView, params || {});
+        girder.events.trigger('g:navigateTo', covalic.views.ChallengeView, {
+            challenge: challenge
+        });
     }, this).on('g:error', function () {
-        girder.router.navigate('challenges', {trigger: true});
+        covalic.router.navigate('challenges', {trigger: true});
     }, this).fetch();
 });

web_external/js/views/body/ChallengesView.js
@@ -3,7 +3,7 @@ covalic.views.ChallengesView = covalic.View.extend({
     events: {
         'click a.c-challenge-link': function (event) {
             var cid = $(event.currentTarget).attr('c-challenge-cid');
-            girder.router.navigate('challenge/' + this.collection.get(cid).id, {trigger: true});
+            covalic.router.navigate('challenge/' + this.collection.get(cid).id, {trigger: true});
         }
     },
 
@@ -15,18 +15,14 @@ covalic.views.ChallengesView = covalic.View.extend({
         }, this).fetch();
     },
 
-
     render: function() {
         this.$el.html(covalic.templates.challengeList({challenges: this.collection.models}));
 
         return this;
     }
-
-
-
 });
 
-girder.router.route('challenges', 'challenges', function (params) {
+covalic.router.route('challenges', 'challenges', function (params) {
     girder.events.trigger('g:navigateTo', covalic.views.ChallengesView, params || {});
     girder.events.trigger('g:highlightItem', 'ChallengesView');
 });

web_external/js/app.js
@@ -1,4 +1,4 @@
-covalic.App = covalic.App.extend({
+covalic.App = girder.App.extend({
 
     render: function() {
         this.$el.html(girder.templates.layout());
@@ -28,4 +28,3 @@ covalic.App = covalic.App.extend({
     },
 
 });
-

web_external/js/init.js
@@ -1,2 +1,9 @@
-// covalic extends girder but doesn't pollute it
-var covalic = _.extend({}, girder);
+var covalic = covalic || {};
+
+_.extend(covalic, {
+    models: {},
+    collections: {},
+    views: {},
+    router: new Backbone.Router(),
+    events: _.clone(Backbone.Events)
+});

web_external/js/views/body/FrontPageView.js
@@ -19,6 +19,6 @@ covalic.views.FrontPageView = girder.views.FrontPageView.extend({
     }
 });
 
-girder.router.route('', 'index', function () {
+covalic.router.route('', 'index', function () {
     girder.events.trigger('g:navigateTo', covalic.views.FrontPageView);
 });

commit f639ea3cbd1f48c1e11a1729ea184b9c6645f628

Fixes #87. Ensure routing works as expected using "covalic.router"
Fixes regression introduced by 41d1b90

This will restore the well functioning of these actions:
* Creating a new challenge redirects to the challenge page
* Deleting a challenge redirects to the challenges list
* Searching challenge
* Deleting a phase redirect to the challenge page
* Submission failure redirects to the challenges list
* Clicking on user setting

Inspired-by: Zach Mullen <zach.mullen@kitware.com>
Thanks: Michael Grauer <michael.grauer@kitware.com>

web_external/js/views/body/ChallengeView.js
@@ -53,7 +53,7 @@ covalic.views.ChallengeView = covalic.View.extend({
                             type: 'success',
                             timeout: 4000
                         });
-                        girder.router.navigate('challenges', {trigger: true});
+                        covalic.router.navigate('challenges', {trigger: true});
                     });
                 }, this)
             });

web_external/js/views/body/ChallengesView.js
@@ -54,14 +54,14 @@ covalic.views.ChallengesView = covalic.View.extend({
             el: $('#g-dialog-container'),
             parentView: this
         }).on('g:saved', function (challenge) {
-            girder.router.navigate('challenge/' + challenge.get('_id'), {
+            covalic.router.navigate('challenge/' + challenge.get('_id'), {
                 trigger: true
             });
         }, this).render();
     },
 
     _gotoChallenge: function (challenge) {
-        girder.router.navigate('challenge/' + challenge.id, {trigger: true});
+        covalic.router.navigate('challenge/' + challenge.id, {trigger: true});
     }
 });
 

web_external/js/views/body/PhaseView.js
@@ -63,7 +63,7 @@ covalic.views.PhaseView = covalic.View.extend({
                             type: 'success',
                             timeout: 4000
                         });
-                        girder.router.navigate(
+                        covalic.router.navigate(
                             'challenge/' + this.model.get('challengeId'), {
                                 trigger: true
                         });

web_external/js/views/body/SubmissionView.js
@@ -144,6 +144,6 @@ covalic.router.route('submission/:id', 'phase_submission', function (id, params)
             submission: submission
         });
     }).on('g:error', function () {
-        girder.router.navigate('challenges', {trigger: true});
+        covalic.router.navigate('challenges', {trigger: true});
     }).fetch();
 });

web_external/js/views/layout/HeaderUserView.js
@@ -21,7 +21,7 @@ covalic.views.LayoutHeaderUserView = covalic.View.extend({
         },
 
         'click a.g-my-settings':function(){
-            girder.router.navigate('useraccount/'+girder.currentUser.get('_id')+
+            covalic.router.navigate('useraccount/'+girder.currentUser.get('_id')+
                                     '/info',{trigger:true});
         }
     },