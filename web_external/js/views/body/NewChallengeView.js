595b06aa3e45ce2ce3f1c2c2820f3a87e65b2c24
Zach Mullen <zach.mullen@kitware.com>
2015-10-15 20:29:48 -0400

    Start of a challenge creation wizard

--- web_external/js/views/body/NewChallengeView.js
+++ web_external/js/views/body/NewChallengeView.js
@@ -1,7 +1,28 @@
 covalic.views.NewChallenge = covalic.View.extend({
+    events: {
+    },
 
+    initialize: function (settings) {
+        this.wizard = settings.wizard || false;
+        if (this.wizard && !_.has(this.wizard, 'current')) {
+            this.wizard.current = 0;
+        }
+
+        this.render();
+    },
+
+    render: function () {
+        this.$el.html(covalic.templates.newChallengePage({
+            wizard: this.wizard
+        }));
+        return this;
+    }
 });
 
 covalic.router.route('challenge/new', 'newChallenge', function () {
-    girder.events.trigger('g:navigateTo', covalic.views.NewChallenge);
+    girder.events.trigger('g:navigateTo', covalic.views.NewChallenge, {
+        wizard: {
+            total: 4
+        }
+    });
 });
