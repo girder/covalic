c827de575ba85892c74c3fdf5c62d2eed34a346a
Max Smolens <max.smolens@kitware.com>
2016-04-01 14:08:45 -0400

    Fix sorting by overall score on leaderboard
    This overrides the default girder.Collection sort options for a
    SubmissionCollection. By default, a girder.Collection is sorted by the 'name'
    parameter, which does not exist for submissions.

--- web_external/js/collections/SubmissionCollection.js
+++ web_external/js/collections/SubmissionCollection.js
@@ -2,5 +2,8 @@ covalic.collections.SubmissionCollection = girder.Collection.extend({
     resourceName: 'covalic_submission',
     model: covalic.models.SubmissionModel,
 
+    sortField: 'overallScore',
+    sortDir: girder.SORT_DESC,
+
     pageLimit: 100
 });
