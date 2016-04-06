8010d79148f59539d3fdd5e1139bb4a74522b56b
Max Smolens <max.smolens@kitware.com>
2016-04-06 16:52:09 -0400

    Show user link on leaderboard only for phase admins

--- web_external/js/views/widgets/SubmissionListWidget.js
+++ web_external/js/views/widgets/SubmissionListWidget.js
@@ -25,6 +25,7 @@ covalic.views.SubmissionListWidget = covalic.View.extend({
         this.$el.html(covalic.templates.leaderboard({
             submissions: this.collection.models,
             start: this.collection.offset - this.collection.length,
+            phase: this.phase,
             girder: girder,
             moment: moment
         }));
