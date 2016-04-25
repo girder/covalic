93b498e8b5028db8ad6c06f43b11039bd5c07c36
Zach Mullen <zach.mullen@kitware.com>
2016-04-25 16:56:30 -0400

    Backup script for mongodb

---
+++ ansible/roles/mongodb/templates/backup_db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+fname=mongodump_$(date +%Y%m%d_%H%M%S)
+mongodump -h {{ mongo_private_ip }} -d girder -o $fname || exit 1
+echo "Archiving girder_db_dump"
+tar cjvf ${fname}.tar.bz2 $fname && rm -rf $fname
+echo "Uploading girder_db_dump.tar.bz2 to S3 covalic-backup bucket"
+python ./upload_to_s3.py ${fname}.tar.bz2 covalic-backup && rm ${fname}.tar.bz2
--- ansible/utils/provision.sh
+++ ansible/utils/provision.sh
@@ -2,6 +2,11 @@
 # argv:
 # $1: pod name
 # $2: path to covalic_admin.pem
+if [ "$#" -ne 2 ]; then
+  echo "Usage: provision.sh <pod> <path_to_covalic_admin.pem>"
+  exit 1
+fi
+
 ansible-playbook -i plugins/inventory/ec2.py -e pod=$1 \
     --vault-password-file vault-password.txt --private-key=$2 \
     -e ansible_ssh_private_key_file=$2 -e ansible_ssh_user=ubuntu provision.yml
