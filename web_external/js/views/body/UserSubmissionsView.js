9f1efdb3de982d63a5d42c8535e635ab0e258624
Max Smolens <max.smolens@kitware.com>
2016-05-04 15:58:45 -0400

    Add basic submission dashboard
    This adds an administrator interface to view all submissions to a phase. A
    button at the bottom of the phase page routes to this interface.
    
    Currently, the view shows submissions grouped by participant. By default,
    submissions for up to five participants are shown at a time. The controls at the
    top of the interface page through the list. The 'participantLimit' query
    parameter sets the number of participants per page.
    
    By default, up to five submissions per participant are shown at a time. The
    controls in the group for each user page through the list. The 'submissionLimit'
    query parameter sets the number of submissions to show per page.
    
    Each submission shows its title, whether it's the latest successful submission,
    its job status, its creation date, and its overall score. The job status text
    links to the job detail page. Unsuccessful jobs include a tooltip showing the
    first few lines of the job log. The overall score text links to the score detail
    page. Links are provided to download the submission data and to view the raw
    submission JSON.
    
    To provide some extra information, warning messages are shown when a user hasn't
    submitted at all or when the current page shows no successful submissions.
    
    Some useful extensions, perhaps requiring backend enhancements to be efficient,
    to this could include:
    - display success/error/total submission counts per user/phase/challenge
    - identify and group participants who have no successful submissions
    - identify and group participants who haven't submitted
    - filtering/sorting tables

---
+++ web_external/js/views/body/UserSubmissionsView.js
@@ -0,0 +1,90 @@
+/**
+* View for a user's submissions to a phase.
+*/
+covalic.views.UserSubmissionsView = covalic.View.extend({
+    initialize: function (settings) {
+        this.phase = settings.phase;
+        this.user = settings.user;
+
+        var submissionLimit = 5;
+        if (!_.isNull(settings.submissionLimit)) {
+            submissionLimit = Math.max(1, parseInt(settings.submissionLimit, 10));
+        }
+
+        new girder.views.LoadingAnimation({
+            el: this.$el,
+            parentView: this
+        }).render();
+
+        // XXX: add way to get total number of submissions without fetching all
+        this.submissions = new covalic.collections.SubmissionCollection();
+        this.submissions.sortField = 'created';
+        this.submissions.sortDir = girder.SORT_DESC;
+        this.submissions.pageLimit = submissionLimit;
+        this.submissions.on('g:changed', function () {
+            // Assume submissions that have overallScore were successful.
+            // Fetch jobs for all other submissions.
+            var unscoredSubmissions =
+                _.filter(this.submissions.models, function (submission) {
+                    return _.isUndefined(submission.get('overallScore'));
+                });
+            if (unscoredSubmissions.length > 0) {
+                var jobs = [];
+                var promises = [];
+                _.each(unscoredSubmissions, function (submission) {
+                    var deferred = $.Deferred();
+                    var job = new girder.models.JobModel({
+                        _id: submission.get('jobId')
+                    }).on('g:fetched', function () {
+                        deferred.resolve();
+                    }, this).on('g:error', function () {
+                        deferred.reject();
+                    }, this).fetch();
+                    jobs.push(job);
+                    promises.push(deferred.promise());
+                });
+                $.when.apply($, promises).done(_.bind(function () {
+                    var jobMap = _.indexBy(jobs, 'id');
+                    this.render({jobs: jobMap});
+                }, this));
+            } else {
+                this.render();
+            }
+        }, this).fetch({
+            phaseId: this.phase.id,
+            userId: this.user.id
+        });
+
+        this.paginateWidget = new girder.views.PaginateWidget({
+            collection: this.submissions,
+            parentView: this
+        });
+    },
+
+    render: function (params) {
+        this.$el.html(covalic.templates.userSubmissionsPage({
+            user: this.user,
+            submissions: this.submissions,
+            girder: girder,
+            moment: moment,
+            jobs: params && params.jobs
+        }));
+
+        this.paginateWidget.setElement(this.$('.c-user-submissions-pagination')).render();
+
+        var tooltipParams = {
+            container: this.$el,
+            animation: false,
+            delay: {show: 100}
+        };
+
+        this.$('.c-tooltip').tooltip(tooltipParams);
+
+        // Override tooltip template to set custom inner class
+        this.$('.c-job-log-tooltip').tooltip(_.extend(tooltipParams, {
+            template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner c-job-log-tooltip-inner"></div></div>'
+        }));
+
+        return this;
+    }
+});
