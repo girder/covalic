026ae8c16a1b8203338098447d1e961d4972459a
Zach Mullen <zach.mullen@kitware.com>
2017-01-17 13:53:13 -0500

    Allow backup_db.sh to be run in other dirs

--- ansible/roles/mongodb/templates/backup_db.sh
+++ ansible/roles/mongodb/templates/backup_db.sh
@@ -4,4 +4,4 @@ mongodump -h {{ mongo_private_ip }} -d girder -o $fname || exit 1
 echo "Archiving girder_db_dump"
 tar cjvf ${fname}.tar.bz2 $fname && rm -rf $fname
 echo "Uploading girder_db_dump.tar.bz2 to S3 covalic-backup bucket"
-python ./upload_to_s3.py ${fname}.tar.bz2 covalic-backup && rm ${fname}.tar.bz2
+python `dirname $0`/upload_to_s3.py ${fname}.tar.bz2 covalic-backup && rm ${fname}.tar.bz2
