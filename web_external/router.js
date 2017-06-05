c8e5dd0b13aaaca8021f55ec2692b1acf20b9a4b
Max Smolens <max.smolens@kitware.com>
2017-06-05 15:13:48 -0400

    Fix eslint_covalic test
    Fix failing eslint_covalic test with girder@91b5db6.

--- web_external/router.js
+++ web_external/router.js
@@ -5,6 +5,29 @@ import girderRouter from 'girder/router';
 import { AccessType } from 'girder/constants';
 import { parseQueryString } from 'girder/misc';
 
+import ChallengeAccessView from './views/body/ChallengeAccessView';
+import ChallengeInstructionsView from './views/body/ChallengeInstructionsView';
+import ChallengeLearnView from './views/body/ChallengeLearnView';
+import ChallengeModel from './models/ChallengeModel';
+import ChallengeThumbnailView from './views/body/ChallengeThumbnailView';
+import ChallengeView from './views/body/ChallengeView';
+import ChallengesView from './views/body/ChallengesView';
+import FrontPageView from './views/body/FrontPageView';
+import NewChallengeView from './views/body/NewChallengeView';
+import NewPhaseView from './views/body/NewPhaseView';
+import PhaseAccessView from './views/body/PhaseAccessView';
+import PhaseConfigureSubmissionsView from './views/body/PhaseConfigureSubmissionsView';
+import PhaseGroundTruthView from './views/body/PhaseGroundTruthView';
+import PhaseInputView from './views/body/PhaseInputView';
+import PhaseInstructionsView from './views/body/PhaseInstructionsView';
+import PhaseModel from './models/PhaseModel';
+import PhaseSubmissionsView from './views/body/PhaseSubmissionsView';
+import PhaseView from './views/body/PhaseView';
+import ScoringMetricsView from './views/body/ScoringMetricsView';
+import SubmissionModel from './models/SubmissionModel';
+import SubmissionView from './views/body/SubmissionView';
+import SubmitView from './views/body/SubmitView';
+
 girderRouter.enabled(false);
 
 var router = new Backbone.Router();
@@ -12,12 +35,10 @@ var router = new Backbone.Router();
 export default router;
 
 // Setup app routes
-import FrontPageView from './views/body/FrontPageView';
 router.route('', 'index', function () {
     events.trigger('g:navigateTo', FrontPageView);
 });
 
-import ChallengesView from './views/body/ChallengesView';
 router.route('challenges', 'challenges', function (params) {
     params = parseQueryString(params);
 
@@ -31,8 +52,6 @@ router.route('challenges', 'challenges', function (params) {
     });
 });
 
-import ChallengeView from './views/body/ChallengeView';
-import ChallengeModel from './models/ChallengeModel';
 router.route('challenge/:id', 'challenge', function (id) {
     // Fetch the challenge by id, then render the view.
     var challenge = new ChallengeModel();
@@ -47,8 +66,6 @@ router.route('challenge/:id', 'challenge', function (id) {
     }, this).fetch();
 });
 
-import PhaseView from './views/body/PhaseView';
-import PhaseModel from './models/PhaseModel';
 router.route('phase/:id', 'phase', function (id) {
     // Fetch the phase by id, then render the view.
     var phase = new PhaseModel({
@@ -74,12 +91,10 @@ router.route('challenge/n/:name', 'challengeByName', function (name) {
     });
 });
 
-import ChallengeLearnView from './views/body/ChallengeLearnView';
 router.route('challenges/learn', 'challengesLearn', function () {
     events.trigger('g:navigateTo', ChallengeLearnView);
 });
 
-import NewChallengeView from './views/body/NewChallengeView';
 router.route('challenge/new', 'newChallenge', function () {
     events.trigger('g:navigateTo', NewChallengeView, {
         wizard: {
@@ -88,7 +103,6 @@ router.route('challenge/new', 'newChallenge', function () {
     });
 });
 
-import NewPhaseView from './views/body/NewPhaseView';
 router.route('challenge/:id/phase/new', 'newPhase', function (id) {
     var challenge = new ChallengeModel({
         _id: id
@@ -101,7 +115,6 @@ router.route('challenge/:id/phase/new', 'newPhase', function (id) {
     });
 });
 
-import ScoringMetricsView from './views/body/ScoringMetricsView';
 router.route('phase/:id/metrics', 'phaseMetrics', function (id) {
     // Fetch the phase by id, then render the view.
     var phase = new PhaseModel({
@@ -116,7 +129,6 @@ router.route('phase/:id/metrics', 'phaseMetrics', function (id) {
     phase.fetch();
 });
 
-import SubmitView from './views/body/SubmitView';
 router.route('phase/:id/submit', 'phase_submit', function (id) {
     var phase = new PhaseModel({
         _id: id
@@ -130,8 +142,6 @@ router.route('phase/:id/submit', 'phase_submit', function (id) {
     }, this).fetch();
 });
 
-import SubmissionView from './views/body/SubmissionView';
-import SubmissionModel from './models/SubmissionModel';
 router.route('submission/:id', 'phase_submission', function (id) {
     var submission = new SubmissionModel({
         _id: id
@@ -145,7 +155,6 @@ router.route('submission/:id', 'phase_submission', function (id) {
     }).fetch();
 });
 
-import PhaseSubmissionsView from './views/body/PhaseSubmissionsView';
 router.route('phase/:id/submissions', 'phaseSubmissions', function (id, params) {
     var phase = new PhaseModel({
         _id: id
@@ -201,26 +210,11 @@ var _wizardPage = function (route, routeName, modelType, viewType) {
     });
 };
 
-import ChallengeThumbnailView from './views/body/ChallengeThumbnailView';
 _wizardPage('challenge/:id/thumbnail', 'challengeThumbnail', ChallengeModel, ChallengeThumbnailView);
-
-import ChallengeInstructionsView from './views/body/ChallengeInstructionsView';
 _wizardPage('challenge/:id/instructions', 'challengeAccess', ChallengeModel, ChallengeInstructionsView);
-
-import ChallengeAccessView from './views/body/ChallengeAccessView';
 _wizardPage('challenge/:id/access', 'challengeAccess', ChallengeModel, ChallengeAccessView);
-
-import PhaseAccessView from './views/body/PhaseAccessView';
 _wizardPage('phase/:id/access', 'phaseAccess', PhaseModel, PhaseAccessView);
-
-import PhaseInstructionsView from './views/body/PhaseInstructionsView';
 _wizardPage('phase/:id/instructions', 'phaseInstructions', PhaseModel, PhaseInstructionsView);
-
-import PhaseInputView from './views/body/PhaseInputView';
 _wizardPage('phase/:id/input', 'phaseInput', PhaseModel, PhaseInputView);
-
-import PhaseGroundTruthView from './views/body/PhaseGroundTruthView';
 _wizardPage('phase/:id/groundtruth', 'phaseGroundTruth', PhaseModel, PhaseGroundTruthView);
-
-import PhaseConfigureSubmissionsView from './views/body/PhaseConfigureSubmissionsView';
 _wizardPage('phase/:id/configure_submissions', 'phaseConfigureSubmission', PhaseModel, PhaseConfigureSubmissionsView);
